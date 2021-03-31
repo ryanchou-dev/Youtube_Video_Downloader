2a940dfd48f8e8b52989524bd69cca4f62496ce9
Ryan Chou <81596991+ryanchou-dev@users.noreply.github.com>
2021-03-30 19:22:21 -0700

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,53 @@
+from pytube import YouTube
+
+import PySimpleGUI as sg
+error_message1 = ""
+sg.change_look_and_feel('bluemono')
+
+layout=[
+    [sg.Text("What video do you want to download? (paste the link)")],
+    [sg.InputText(key='link')],
+    [sg.Text(error_message1)],
+    [sg.Button('Submit'), sg.Button('Cancel')]
+]
+
+window = sg.Window("Youtube Video Downloader", layout, resizable=True, element_justification="c").Finalize()
+window.Maximize()
+
+while True:
+    event, value = window.Read()
+    if event in (None, 'Cancel'):
+        break
+    if event == 'Submit':
+        try:
+            link = value['link']
+            yt = YouTube(link)
+            ys = yt.streams.get_by_itag('18')
+            ys.download('Downloads')
+            window.Close()
+            error_message1 = "Completed. Check your Downloads Folder."
+            layout=[
+                [sg.Text("What video do you want to download? (paste the link)")],
+                [sg.InputText(key='link')],
+                [sg.Text(error_message1)],
+                [sg.Button('Submit'), sg.Button('Cancel')]
+            ]
+
+            window = sg.Window("Youtube Video Downloader", layout, resizable=True, element_justification="c").Finalize()
+            window.Maximize()
+            event, value = window.Read()
+        except:
+            error_message1 = "Couldn't read that, try again."
+            window.Close()
+            layout=[
+                [sg.Text("What video do you want to download? (paste the link)")],
+                [sg.InputText(key='link')],
+                [sg.Text(error_message1)],
+                [sg.Button('Submit'), sg.Button('Cancel')]
+            ]
+
+            window = sg.Window("Youtube Video Downloader", layout, resizable=True, element_justification="c").Finalize()
+            window.Maximize()
+            event, value = window.Read()
+
+
